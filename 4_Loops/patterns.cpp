fe7c32f0beb8b8da17a527a5210a45f7c57d4442
Nipun Shrivats <nipunrawat16@gmail.com>
2025-01-19 21:21:46 +0530

    butterfly pattern

--- 4_Loops/patterns.cpp
+++ 4_Loops/patterns.cpp
@@ -560,5 +560,75 @@ int main()
 
     // ------------------------------------------
 
+    // 20  Butter fly pattern
+
+    // *      *
+    // **    **
+    // ***  ***
+    // ********
+    // ********
+    // ***  ***
+    // **    **
+    // *      *
+
+    // int n;
+    // cout << "Enter number: ";
+    // cin >> n;
+
+    // // upper portion
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     for (int k = 0; k < (n - i - 1); k++)
+    //     {
+    //         cout << " ";
+    //     }
+
+    //     // cout << " ";
+
+    //     for (int k = 0; k < (n - i - 1); k++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    // // lower portion
+
+    // for (int i = 0; i < n; i++)
+    // {
+
+    //     for (int j = 0; j < n - i; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     for (int k = 0; k < i; k++)
+    //     {
+    //         cout << " ";
+    //     }
+
+    //     // cout << " ";
+
+    //     for (int k = 0; k < i; k++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int j = 0; j < n - i; j++)
+    //     {
+    //         cout << "*";
+    //     }
+
+    //     cout << endl;
+    // }
+
+    // ------------------------------------------
+
     return 0;
 }
--- 4_Loops/tempCodeRunnerFile.cpp
+++ 4_Loops/tempCodeRunnerFile.cpp
@@ -523,40 +523,103 @@ int main()
     //     * *
     //      *
 
+    // int n;
+    // cout << "Enter number: ";
+    // cin >> n;
+
+    // // part-1 //
+    // for (int l = 0; l < n; l++)
+    // {
+    //     cout << "* ";
+    // }
+    // cout << endl;
+
+    // // Part-2 //
+    // for (int i = 0; i < n - 1; i++)
+    // {
+
+    //     // space-1
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     cout << "*";
+
+    //     // space-2
+    //     if (i != n - 2)
+    //     {
+
+    //         for (int j = 0; j < (2 * (n - 2 - i) - 1); j++)
+    //         {
+    //             cout << " ";
+    //         }
+    //         cout << "*";
+    //         cout << endl;
+    //     }
+    // }
+
+    // ------------------------------------------
+
+    // 20  Butter fly pattern
+
     int n;
     cout << "Enter number: ";
     cin >> n;
 
-    // part-1 //
-    for (int l = 0; l < n; l++)
+    // upper portion
+    for (int i = 0; i < n; i++)
     {
-        cout << "* ";
-    }
-    cout << endl;
-
-    // Part-2 //
-    for (int i = 0; i < n - 1; i++)
-    {
-
-        // space-1
         for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int k = 0; k < (n - i - 1); k++)
         {
             cout << " ";
         }
-        cout << "*";
 
-        // space-2
-        if (i != n - 2)
+        // cout << " ";
+
+        for (int k = 0; k < (n - i - 1); k++)
         {
-
-            for (int j = 0; j < (2 * (n - 2 - i) - 1); j++)
-            {
-                cout << " ";
-            }
-            cout << "*";
-            cout << endl;
+            cout << " ";
         }
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
     }
+
+    // lower portion
+
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+        for (int k = 0; k < i; k++)
+        {
+            cout << " ";
+        }
+
+        // cout << " ";
+
+        for (int k = 0; k < i; k++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // ------------------------------------------
 
     return 0;
 }
